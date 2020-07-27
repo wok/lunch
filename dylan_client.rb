6da9874960697108e278d5484e51c62771c775cb
Wolfgang Kölbl <wolfgang.kolbl@taikala.fi>
2020-07-27 11:25:43 +0300

    Fix dylan

--- dylan_client.rb
+++ dylan_client.rb
@@ -3,7 +3,7 @@ class DylanClient < FoodClient
   def initialize(restaurant, date = Date.today)
     @date = date
     @restaurant_name = "Dylan #{restaurant}"
-    @restaurant_url = "https://www.dylan.fi/#{restaurant}-lunch-menu/"
+    @restaurant_url = "https://www.dylan.fi/#{restaurant}"
   end
   
   def load_menus
@@ -17,26 +17,16 @@ class DylanClient < FoodClient
     end
     content = content.gsub('&nbsp;', '').gsub('&shy;', '')
     page = Nokogiri::HTML(content)
-    lines = []
-    page.css('.sqs-block-html .sqs-block-content').xpath('//p').each do |section|
-      next if section.inner_html.start_with?('<a target=')
-      lines = lines + section.inner_html.split('<br>')
-    end
-    
-    # find current day
-    is_current = false
-    day_lines = []
-    lines.each do |line|
-      if line.start_with?('<strong>')
-        is_current = line.start_with?("<strong>#{local_weekday}")
-      else
-        day_lines.push(line&.gsub('&nbsp;', '')) if is_current
-      end
-    end
+    container = page.at('//div[contains(.//span, "LOUNAS")]').next
+
     @menus = []
-    day_lines.each do |line|
-      line = line.split('/').first&.strip&.gsub('<\/em>', '')&.gsub('<em>', '')&.gsub('<', '')
-      @menus.push(line) unless line.nil? || line.length <= 5
+    container.xpath('//div[@role="gridcell"]').each do |cell|
+      day = cell.css('h4').first&.text
+      next unless day&.start_with?(local_weekday)
+
+      @menus = cell.css('p span span').text.split("\n")
     end
+
+    puts @menus
   end
 end
